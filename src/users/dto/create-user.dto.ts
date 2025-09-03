76ebe35fa88834bbfa40a1735da0b46dee862bcc
Chagas011 <washington.chagas.9@hotmail.com>
2025-09-03 20:26:32 -0300

    feat: add create function users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+	@IsString()
+	@IsNotEmpty()
+	name: string;
+
+	@IsEmail()
+	@IsNotEmpty()
+	@IsString()
+	email: string;
+
+	@IsNotEmpty()
+	@IsString()
+	@MinLength(8)
+	password: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,74 @@
-import { Injectable } from "@nestjs/common";
+import {
+	ConflictException,
+	Injectable,
+	NotFoundException,
+} from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
-import { UpdateUserDto } from "./dto/update-user.dto";
+import { hash } from "bcryptjs";
+import { PrismaService } from "src/database/prisma.service";
 
 @Injectable()
 export class UsersService {
-	create(createUserDto: CreateUserDto) {
-		return "This action adds a new user";
+	constructor(private prisma: PrismaService) {}
+
+	async create(createUserDto: CreateUserDto) {
+		const emailIsExist = await this.prisma.user.findUnique({
+			where: {
+				email: createUserDto.email,
+			},
+		});
+
+		if (emailIsExist) {
+			throw new ConflictException("This email is already in use");
+		}
+		const hashedPassword = await hash(createUserDto.password, 12);
+		const user = await this.prisma.user.create({
+			data: {
+				...createUserDto,
+				password: hashedPassword,
+				categories: {
+					createMany: {
+						data: [
+							{ name: "Salario", icon: "travel", type: "INCOME" },
+							{ name: "Freelance", icon: "freelance", type: "INCOME" },
+							{ name: "Outro", icon: "other", type: "INCOME" },
+
+							{ name: "Aluguel", icon: "home", type: "EXPENSE" },
+							{ name: "Mercado", icon: "shopping", type: "EXPENSE" },
+							{ name: "Transporte", icon: "car", type: "EXPENSE" },
+							{ name: "Restaurante", icon: "food", type: "EXPENSE" },
+							{ name: "Saúde", icon: "health", type: "EXPENSE" },
+							{ name: "Educação", icon: "education", type: "EXPENSE" },
+							{ name: "Lazer", icon: "entertainment", type: "EXPENSE" },
+							{ name: "Contas de Casa", icon: "utilities", type: "EXPENSE" },
+							{ name: "Assinaturas", icon: "subscription", type: "EXPENSE" },
+							{ name: "Outros", icon: "other", type: "EXPENSE" },
+						],
+					},
+				},
+			},
+		});
+
+		return user;
 	}
 
-	findAll() {
-		return `This action returns all users`;
-	}
+	async findOne(id: string) {
+		const user = await this.prisma.user.findUnique({
+			where: {
+				id,
+			},
+			select: {
+				name: true,
+				email: true,
+				categories: true,
+			},
+		});
 
-	findOne(id: number) {
-		return `This action returns a #${id} user`;
-	}
+		if (!user) {
+			throw new NotFoundException("User not found");
+		}
 
-	update(id: number, updateUserDto: UpdateUserDto) {
-		return `This action updates a #${id} user`;
+		return user;
 	}
 
 	remove(id: number) {
