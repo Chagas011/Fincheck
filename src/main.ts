153dfcbc45bbb10dce82f079343069623c85e80f
Chagas011 <washington.chagas.9@hotmail.com>
2025-09-24 20:11:07 -0300

    configurando cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	app.useGlobalPipes(new ValidationPipe());
+	app.enableCors({
+		origin: "*",
+	});
 	app.useGlobalGuards();
 	await app.listen(process.env.PORT ?? 3000);
 }
